2cf013f4dd9512f80bca4e6299ba4179a1f55bbd
blacksmith96 <blacksmith96@yahoo.com>
2018-07-22 20:27:09 -0400

    完成最基础的API设计

--- BTree/library.h
+++ BTree/library.h
@@ -1,6 +1,35 @@
 #ifndef BTREE_LIBRARY_H
 #define BTREE_LIBRARY_H
 
-void hello();
+#include <vector>
+
+const int N = 2;    //N is minimum degree
+
+template <typename KEY, typename  VALUE>
+typedef struct node{
+    KEY index;
+    VALUE v;
+    std::vector<KEY> keys;
+    std::vector<struct node *> pchildren;
+} NODE;
+
+template <typename KEY, typename VALUE>
+class BTree{
+public:
+    // Construction Function
+    BTree();
+
+    // Decomposition Function
+    ~BTree();
+
+    // Insert (key,value) pair
+    void Insert(KEY key, VALUE value);
+
+    // judge a given key's existence
+    bool find(KEY key);
+
+private:
+    NODE<KEY,VALUE> * root;
+};
 
 #endif
