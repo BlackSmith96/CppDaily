1b12a45af94427005631605489e3dc3b185ec776
blacksmith96 <blacksmith96@yahoo.com>
2018-08-18 15:03:01 -0400

    完成一道CppPrimer题目

--- CppPrimerExercises/main.cpp
+++ CppPrimerExercises/main.cpp
@@ -5,7 +5,10 @@
 using namespace std;
 
 int main() {
-    vector<int> v1 = {1,2,3,4,5}, v2 = {1,2,3,4,5};
-    cout << (v1 == v2) << endl;
+    list<int> l{1,2,3,4};
+    vector<int> v{1,2,3,5};
+    vector<int> t;
+    t.assign(l.begin(),l.end());
+    cout << (v == t?"true":"false") << endl;
     return 0;
 }
